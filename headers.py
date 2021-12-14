62be7bee610e8e3d82fae30a4a126391213a224c
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-12-14 03:42:19 +0300

    Create headers.py

---
+++ headers.py
@@ -0,0 +1,23 @@
+print("""
++--------------------------+
++ Python Headers  View     +
++  github.com/hawk-unity   +
++  instagram : coder.faruk +
++---------------------------
+youtube: farukdeveloper.online
+-------------------------------
+- Http Header / Link Extractor
+""")
+#!/usr/bin/python3
+import sys
+from requests import get
+import subprocess
+import json
+url = input(" URL  (google.com)   ->   ")
+response = get('https://api.hackertarget.com/httpheaders/?q=' + url).text
+sys.stdout.write(response)
+print("------------------------------LİNK EXTRACTOR---------------------------------------------------------->
+print("------------------------------LİNK EXTRACTOR---------------------------------------------------------->
+print("------------------------------LİNK EXTRACTOR---------------------------------------------------------->
+print("------------------------------LİNK EXTRACTOR---------------------------------------------------------->
+response2 = get('https://api.hackertarget.com/pagelinks/?q=' + url).text
